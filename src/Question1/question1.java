4b4b92ea26e7b3df24ac47cb3a320f0a2add4029
Khraos Genetor <khraos.genetor@gmail.com>
2024-07-12 18:46:03 +0530

    Welp new commit

--- src/Question1/question1.java
+++ src/Question1/question1.java
@@ -1,2 +1,59 @@
-package Question1;public class question1 {
+package Question1;
+
+import java.util.Scanner;
+
+class test {
+    void print() {
+        int num = 1;
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(num+" ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    boolean print(int num) {
+        int temp = num, sum = 0;
+        while( temp != 0 ){
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if((Math.pow(sum, 3)) == num) {
+            return true;
+        }
+        return false;
+    }
+}
+
+public class question1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        test t = new test();
+        System.out.print("Do you want to print pattern or check for Dudeney number?\nType A for pattern, and B for Dudeney number:");
+        char ch = in.next().charAt(0);
+        switch (ch) {
+            case 'A':
+            case 'a':
+                t.print();
+                break;
+
+            case 'B':
+            case 'b':
+                System.out.print("Enter the number you wish to check: ");
+                int n = in.nextInt();
+                if(t.print(n)){
+                    System.out.println(n+" is a Dudeney number.");
+                } else {
+                    System.out.println(n+" is not a Dudeney number.");
+                }
+                break;
+
+            default:
+                System.err.println("Not a valid choice: "+ch);
+                System.exit(400);
+        }
+        in.close();
+    }
 }
--- src/Question1/question2.java
+++ src/Question1/question2.java
@@ -1,2 +1,63 @@
-package Question1;public class question2 {
+package Question1;
+
+import java.util.Scanner;
+
+class My_Rating {
+    String P_Name, Quality;
+    int Star1, Star2, Star3, Star4, Star5;
+    float Average_Rating;
+    My_Rating(String name) {
+        Star1 = 0;
+        Star2 = 0;
+        Star3 = 0;
+        Star4 = 0;
+        Star5 = 0;
+        P_Name = name;
+        Quality = null;
+        Average_Rating = 0.0f;
+    }
+    void Find_Rating() {
+        Scanner in = new Scanner(System.in);
+        System.out.print("How many people gave 1 star: ");
+        Star1 = in.nextInt();
+        System.out.print("How many people gave 2 stars: ");
+        Star2 = in.nextInt();
+        System.out.print("How many people gave 3 stars: ");
+        Star3 = in.nextInt();
+        System.out.print("How many people gave 4 stars: ");
+        Star4 = in.nextInt();
+        System.out.print("How many people gave 5 stars: ");
+        Star5 = in.nextInt();
+        Average_Rating = ((1.0f * Star1) + (2.0f * Star2) + (3.0f * Star3) + (4.0f * Star4) + (5.0f * Star5)) /
+                (Star1 + Star2 + Star3 + Star4 + Star5);
+        if(Average_Rating >= 1.00 && Average_Rating <= 1.79) {
+            Quality = "Fair";
+        } else if (Average_Rating >= 1.80 && Average_Rating <= 2.59) {
+            Quality = "Good";
+        } else if (Average_Rating >= 2.60 && Average_Rating <= 3.39) {
+            Quality = "Very Good";
+        } else if (Average_Rating >= 3.40 && Average_Rating <= 4.19) {
+            Quality = "Excellent";
+        } else if (Average_Rating >= 4.20 && Average_Rating <= 5.00) {
+            Quality = "Exceptional";
+        } else {
+            System.err.println("Invalid Rating: "+Average_Rating);
+            System.exit(400);
+        }
+        System.out.println("The product name: "+P_Name);
+        System.out.println("The average rating: "+Average_Rating);
+        System.out.println("The quality: "+Quality);
+    }
+}
+
+
+public class question2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String name;
+        System.out.print("Enter the product name: ");
+        name = in.nextLine();
+        My_Rating rating = new My_Rating(name);
+        rating.Find_Rating();
+    }
 }
